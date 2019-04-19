5661a5e60fd9e5479f4866b54912296d3658b12a
Renato Cavalcanti <renato@cavalcanti.be>
2019-04-19 11:12:43 +0200

    Adds test for scaladsl

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,31 +9,33 @@ object Dependencies {
   object Versions {
     val Lagom = sys.props.getOrElse("lagom.build.version", "1.5.0")
     val Akka = "2.5.22"
-    val ScalaTest = "3.0.7"
   }
 
-  val akkaDiscovery = "com.typesafe.akka" %% "akka-discovery" % Versions.Akka
-
-  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.26" % Provided
-  val lagomJavadslClient = "com.lightbend.lagom" %% "lagom-javadsl-client" % Versions.Lagom % Provided
-  val lagomScaladslClient = "com.lightbend.lagom" %% "lagom-scaladsl-client" % Versions.Lagom % Provided
-
-  val scalaTest = "org.scalatest" %% "scalatest" % Versions.ScalaTest % Test
-  val logback = "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % Test
 
   val serviceLocatorCore = Seq(
-    akkaDiscovery,
-    slf4j,
+    "com.typesafe.akka" %% "akka-discovery" % Versions.Akka,
+    "org.slf4j" % "slf4j-api" % "1.7.26" % Provided,
+
     scalaTest,
-    logback
+    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
   )
 
   val serviceLocatorJavadsl = Seq(
-    lagomJavadslClient
+    "com.lightbend.lagom" %% "lagom-javadsl-client" % Versions.Lagom % Provided,
+    
+    // test dependencies
+    scalaTest,
+    "com.lightbend.lagom" %% "lagom-javadsl-testkit" % Versions.Lagom % Test
   )
 
   val serviceLocatorScaladsl = Seq(
-    lagomScaladslClient
+    "com.lightbend.lagom" %% "lagom-scaladsl-client" % Versions.Lagom % Provided,
+
+    // test dependencies
+    scalaTest,
+    "com.softwaremill.macwire" %% "macros" % "2.3.0" % Test,
+    "com.lightbend.lagom" %% "lagom-scaladsl-testkit" % Versions.Lagom % Test
   )
 
 }
